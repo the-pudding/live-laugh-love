685320992a04e9a2e7411c27a0f1891498a79c80
Jan Diehm <jadiehm@gmail.com>
2021-03-18 15:36:20 -0500

    gets image json

--- process/s3.js
+++ process/s3.js
@@ -11,41 +11,29 @@ const rawDataLength = rawData.length;
 const etsyBase = 'https://openapi.etsy.com/v2/listings/';
 const etsyKeystring = 'o02rgsarzbewnum8cbcoe9p9';
 
-function delay(i) {
-	setTimeout(() => {
-	  console.log(i)
-	}, 5000 * rawDataLength - 5000 * i);
-  }
-
 function loopListings(listing) {
 	const listingID = listing.listingID;
 	getImages(listingID)
 }
 
-async function getImages(listingID) {
+async function getImages(listing) {
+	const listingID = listing.listingID;
 	const etsyURL = `${etsyBase}${listingID}/images?api_key=${etsyKeystring}`
 	return new Promise(resolve =>
 		request(etsyURL, (err, response, body) => {
 			fs.writeFileSync(`${OUT_PATH}id-${listingID}.json`, body)
+			resolve();
 		})
 	)
 }
 
 //Initial function (THIS IS THE ONLY FUNCTION BEING CALLED - PLACE THINGS INSIDE OF ME!)
 async function init() {
-	let i = 0;
-	
-	for (const listing of rawData) {
-		try {
-			await loopListings(listing);
-			await delay(i);
-		} catch (error) {
-			console.log(error);
-		}
-		i += 1;
-	}
+	for (const [index, listing] of rawData.entries()){
+		await getImages(listing).catch(error => console.error(error))
+	 }
 
 }
 
 //RUN THE INIT FUNCTION!
-init();
+init().catch(error => console.error(error));
